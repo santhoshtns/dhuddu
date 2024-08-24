9a1e03c2ba7e976e660f0301bfcf4212d04aa986
Santhosh Sampath <santhosh.sampath@sea.live>
2024-08-25 02:20:22 +0800

    chapt 0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Subscribe to Dhuddu
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const name = "Dhuddu";
+
+  function handleNameChange() {
+    const names = ["Earn", "Grow", "Give"];
+    const int = Math.floor(Math.random()*3);
+    return names[int];
+  }
+
+  return <div>
+    Subscribe to {name}
+    <p>Let's {handleNameChange()} Money</p>
+  </div>;
 }
 
 export default App;
