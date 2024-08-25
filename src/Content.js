9e9fb6b2d7d3f78a43e8bb1dc38c88ce956edb9d
Santhosh Sampath <santhosh.sampath@sea.live>
2024-08-25 11:42:01 +0800

    chapt 4b usestate

--- src/Content.js
+++ src/Content.js
@@ -2,37 +2,20 @@ import React from "react";
 import { useState } from "react";
 
 const Content = () => {
+  const [name, setName] = useState("Earn");
 
-  function handleNameChange() {
+  function subscribeFunction() {
     const names = ["Earn", "Grow", "Give"];
     const int = Math.floor(Math.random() * 3);
-    return names[int];
-  }
-
-  function namee() {
-    return console.log('Visit santhosh')
-  }
-
-  const [count, setCount] = useState(99)
-  const [name, setName] = useState(() => namee())
-
-  const incrementFunction = () => {
-    setCount((prevCount) => prevCount + 1)
-  }
-
-  const decrementFunction = () => {
-    setCount((prevCount) => prevCount - 1)
+    setName(names[int])
   }
 
   return (
     <main>
-      <p > Lets Earn Money </p>
-      <button> Subscribe </button>
-      <button onClick={decrementFunction}>-</button>
-      <span>{count}</span>
-      <button onClick={incrementFunction}>+</button>
+      <p> Lets {name} Money </p>
+      <button onClick={subscribeFunction}> Subscribe </button>
     </main>
-  )
-}
+  );
+};
 
 export default Content;
