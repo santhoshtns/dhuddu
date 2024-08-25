7195a98b8dd5e148fbed707931676caf9fa60036
Santhosh Sampath <santhosh.sampath@sea.live>
2024-08-25 10:58:22 +0800

    chapt4 click events

--- src/Content.js
+++ src/Content.js
@@ -7,9 +7,18 @@ const Content = () => {
     return names[int];
   }
 
+  const handleClick = (e) => {
+    console.log(e.target.innerText)
+  }
+
+  const handleClick2 = (name) => {
+    console.log(`thanks for the support ${name}`)
+  }
+
   return (
     <main>
-      <p> Lets {handleNameChange()} Money </p>
+      <p onDoubleClick={() => handleClick2('Santhosh')}> Lets {handleNameChange()} Money </p>
+      <button onClick={(e) => handleClick(e)}> Subscribe </button>
     </main>
   )
 }
