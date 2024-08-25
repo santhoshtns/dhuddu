e2638aa85364bf4f230114b61d8ef70b35d8a6ac
Santhosh Sampath <santhosh.sampath@sea.live>
2024-08-25 13:33:23 +0800

    chapt7 controllerinputs

---
+++ src/AddItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { FaPlus } from "react-icons/fa";
+
+const AddItem = ({newItem, setNewItem, handleSubmit}) => {
+  return (
+    <form className="addForm" onSubmit={handleSubmit}>
+      <label htmlFor="addItem"></label>
+      <input
+        autoFocus
+        id="addItem"
+        type="text"
+        placeholder="Add Item"
+        required
+        value = {newItem}
+        onChange = {(e) => setNewItem(e.target.value)}
+      ></input>
+      <button type="submit" aria-label="Add Item">
+        <FaPlus />
+      </button>
+    </form>
+  );
+};
+
+export default AddItem;
--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import AddItem from "./AddItem";
 import Content from "./Content";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -9,6 +10,16 @@ function App() {
     { id: 2, checked: false, item: "Play Cricket" },
     { id: 3, checked: false, item: "Read AI" },
   ]);
+
+  const [newItem, setNewItem] = useState('')
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const addNewItem = {id:id, checked:false, item}
+    const listItems = [...items, addNewItem]
+    setItems(listItems)
+    localStorage.setItem("todo_list", JSON.stringify(listItems));
+  }
 
   const handleCheck = (id) => {
     console.log(`item id ${id}`);
@@ -25,12 +36,28 @@ function App() {
     localStorage.setItem("todo_list", JSON.stringify(listItems));
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault() // to prevent page reload behavior html form element
+    console.log('submitted')
+
+    if(!newItem) return;
+
+    addItem(newItem)
+
+    // clear the edit
+    setNewItem('')
+  }
+
   return (
     <div className="App">
       <Header title = "To Do List" />
+      <AddItem
+        newItem = {newItem}
+        setNewItem = {setNewItem}
+        handleSubmit = {handleSubmit}
+       />
       <Content
         items = {items}
-        setItems = {setItems}
         handleCheck = {handleCheck}
         handleDelete = {handleDelete}
        />
