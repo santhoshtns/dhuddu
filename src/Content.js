604d02bcffcf3a180e53cd170b0f41c23d0d1ffd
Santhosh Sampath <santhosh.sampath@sea.live>
2024-08-25 12:26:23 +0800

    chapt5 list keys style

--- src/Content.js
+++ src/Content.js
@@ -1,19 +1,57 @@
 import React from "react";
 import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
 const Content = () => {
-  const [name, setName] = useState("Earn");
+  const [items, setItems] = useState([
+    { id: 1, checked: true, item: "Practice Coding" },
+    { id: 2, checked: false, item: "Play Cricket" },
+    { id: 3, checked: false, item: "Read AI" },
+  ]);
 
-  function subscribeFunction() {
-    const names = ["Earn", "Grow", "Give"];
-    const int = Math.floor(Math.random() * 3);
-    setName(names[int])
-  }
+  const handleCheck = (id) => {
+    console.log(`item id ${id}`);
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
+  };
+
+  const handleDelete = (id) => {
+    console.log(`item id ${id}`);
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem("todo_list", JSON.stringify(listItems));
+  };
 
   return (
     <main>
-      <p> Lets {name} Money </p>
-      <button onClick={subscribeFunction}> Subscribe </button>
+      {items.length ? (
+        <ul>
+          {items.map((item) => (
+            <li className="item" key={item.id}>
+              <input
+                type="checkbox"
+                checked={item.checked}
+                onChange={() => handleCheck(item.id)}
+              ></input>
+              <label
+                style={item.checked ? { textDecoration: "line-through" } : null}
+                onDoubleClick={() => handleCheck(item.id)}
+              >
+                {item.item}
+              </label>
+              <FaTrashAlt
+                role="Button"
+                tabIndex="0"
+                onClick={() => handleDelete(item.id)}
+              ></FaTrashAlt>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p style={ {marginTop:'2rem'} }> The list is empty</p>
+      )}
     </main>
   );
 };
